47aeead6845d6d63c570e0877abb2c60617224e8
Minect86 <kobzeve668@gmail.com>
2025-02-27 15:19:55 +0300

    Добавьте файлы проекта.

---
+++ SpawnTimerExiledSCPSL14/Config.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Interfaces;
+
+namespace SpawnTimerExiledSCPSL14
+{
+    internal class Config : IConfig
+    {
+        public bool IsEnabled {  get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public string ColorNTF { get; set; } = "#6D9FF7";
+        public string ColorChaos { get; set; } = "#608F38";
+        public string TextMiniWaveNTF { get; set; } = "<color=#6D9FF7>Ntf mini wave</color>";
+        public string TextMiniWaveChaos { get; set; } = "<color=#608F38>Chaos mini wave</color>";
+    }
+}
---
+++ SpawnTimerExiledSCPSL14/Plugin.cs
@@ -0,0 +1,87 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Waves;
+using Exiled.Events.EventArgs.Server;
+using MEC;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpawnTimerExiledSCPSL14
+{
+    internal class Plugin : Plugin<Config>
+    {
+        private CoroutineHandle _TimerCoroutine;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted += StartedRound;
+            Exiled.Events.Handlers.Server.RestartingRound += RestartingRound;
+            Exiled.Events.Handlers.Server.RoundEnded += EndedRound;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= StartedRound;
+            Exiled.Events.Handlers.Server.RestartingRound -= RestartingRound;
+            Exiled.Events.Handlers.Server.RoundEnded -= EndedRound;
+            base.OnDisabled();
+        }
+
+        private void StartedRound()
+        {
+            _TimerCoroutine = Timing.RunCoroutine(TimerNumerator());
+        }
+        private void RestartingRound()
+        {
+            if (Timing.IsRunning(_TimerCoroutine))
+                Timing.KillCoroutines(_TimerCoroutine);
+        }
+        private void EndedRound(RoundEndedEventArgs ev)
+        {
+            if (Timing.IsRunning(_TimerCoroutine))
+                Timing.KillCoroutines(_TimerCoroutine);
+        }
+
+        private IEnumerator<float> TimerNumerator()
+        {
+            while (true)
+            {
+                yield return Timing.WaitForSeconds(1);
+                foreach (Player pl in Player.List)
+                {
+                    if ((pl.Role.Type == RoleTypeId.Spectator) && !Round.IsLobby)
+                        pl.Broadcast(1, $"<size=45%><b>{TimeSpawn()}</b></size>");
+                }
+            }
+        }
+
+        private string TimeSpawn()
+        {
+            List<WaveTimer> waves = new List<WaveTimer>()
+            {
+                WaveTimer.GetWaveTimers()[0],
+                WaveTimer.GetWaveTimers()[1]
+            };
+
+            var minTimer = waves.Aggregate((min, next) => next.TimeLeft.TotalSeconds < min.TimeLeft.TotalSeconds ? next : min);
+
+            if (minTimer.Name == "NtfSpawnWave")
+            {
+                if (minTimer.TimeLeft < TimeSpan.FromSeconds(1))
+                    return $"<color=#6D9FF7>Ntf</color>";
+                else
+                    return $"<color=#6D9FF7>{minTimer.TimeLeft.Minutes}:{minTimer.TimeLeft.Seconds:00}</color>";
+            }
+            else if (minTimer.Name == "ChaosSpawnWave")
+            {
+                if (minTimer.TimeLeft < TimeSpan.FromSeconds(1))
+                    return $"<color=#608F38>Chaos</color>";
+                else
+                    return $"<color=#608F38>{minTimer.TimeLeft.Minutes}:{minTimer.TimeLeft.Seconds:00}</color>";
+            }
+
+            return "Error";
+        }
+    }
+}
